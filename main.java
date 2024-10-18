3f6fad9da92981ff1fc1454c8a5ec4372d5363db
LcamposF <luacampod@gmail.com>
2024-10-18 19:52:45 -0300

    Adicionando senha

--- main.java
+++ main.java
@@ -1,5 +1,7 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Digite seu nome: ");
+        System.out.println("Digite seu email");
+        System.out.println("Digite sua senha");
 }
 }
